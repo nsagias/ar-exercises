274d345dd98157e1d882cc02f220a25f9a433434
Nick <nsagias@gmail.com>
2021-11-11 12:55:30 -0500

    Remove unused code

--- lib/store.rb
+++ lib/store.rb
@@ -2,5 +2,4 @@ class Store < ActiveRecord::Base
   has_many :employees
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  # validates :store, presence: true
 end
