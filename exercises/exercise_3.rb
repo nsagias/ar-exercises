ca8f41caee7aab39273787fdf0ad816e9d627fa4
Nick <nsagias@gmail.com>
2021-11-11 11:23:37 -0500

    Add boolean expression to confirm store is two notes and clean code

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -7,9 +7,14 @@ puts "----------"
 
 # Your code goes here ...
 # Exercise 3: Delete the third store
+
+
 # Load the third store (into @store3) as you did the other two before.
-# Using Active Record's destroy method, delete the store from the database.
-# Verify that the store has been deleted by again outputting (putsing) the count (as you did in Exercise 1.)
 @store3 = Store.find(2)
+
+# Using Active Record's destroy method, delete the store from the database.
 @store3.destroy
+
+# Verify that the store has been deleted by again outputting (putsing) the count (as you did in Exercise 1.)
 p "this is the store count #{Store.count}"
+p "this is the store count #{Store.count  == 2}" # true
