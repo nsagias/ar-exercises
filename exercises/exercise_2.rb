c6d7fbf99525d968986777e1cd7288ff1d9d787f
Nick <nsagias@gmail.com>
2021-11-11 11:25:29 -0500

    Add notes and clean code

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,16 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Exercise 2: Update the first store
+
 # Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
-# Load the second store from the database and assign it to @store2.
-# Update the first store (@store1) instance in the database. (Change its name or something.)
-
-
-# Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
 @store1 = Store.find(1)
+
+# Load the second store from the database and assign it to @store2.
 @store2 = Store.find(2)
 p @store1
 p @store2
 
+
+# Update the first store (@store1) instance in the database. (Change its name or something.)
 @store1.update(name: 'Amazon')
 p @store1
