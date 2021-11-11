71485449394e991210c0681901cd3862384105a2
Nick <nsagias@gmail.com>
2021-11-10 23:57:49 -0500

    Create first record exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,7 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+
+
+
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
