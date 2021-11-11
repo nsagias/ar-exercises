071cebbe2db4512aa012aa29cfc92fea5fe0e309
Nick <nsagias@gmail.com>
2021-11-11 00:35:19 -0500

    remove dev code

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -9,25 +9,6 @@ puts "----------"
 # Load the second store from the database and assign it to @store2.
 # Update the first store (@store1) instance in the database. (Change its name or something.)
 
-# ActiveRecord::Schema.define do
-#   drop_table :stores if ActiveRecord::Base.connection.table_exists?(:stores)
-#   drop_table :employees if ActiveRecord::Base.connection.table_exists?(:employees)
-#   create_table :stores do |t|
-#     t.column :name, :string
-#     t.column :annual_revenue, :integer
-#     t.column :mens_apparel, :boolean
-#     t.column :womens_apparel, :boolean
-#     t.timestamps null: false
-#   end
-#   create_table :employees do |table|
-#     table.references :store
-#     table.column :first_name, :string
-#     table.column :last_name, :string
-#     table.column :hourly_rate, :integer
-#     table.timestamps null: false
-#   end
-# end
-# Your code goes here ...
 
 # Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
 @store1 = Store.find(1)
